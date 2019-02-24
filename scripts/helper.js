260a2c2c972809a11666b7b7caf285d9702fd874
Christina <christina.barker.35@gmail.com>
2019-02-24 09:38:26 -0800

    total-time

--- scripts/helper.js
+++ scripts/helper.js
@@ -2,8 +2,7 @@
 class Helper {
   playPauseAndUpdate(song) {
     player.playPause(song);
-    
-  }
+    }
 }
 
 const helper = new Helper();
--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -34,7 +34,7 @@
      if (player.playState !== 'playing') { return; }
      const currentTime = player.getTime();
      const duration = player.getDuration();
-     const percent = (currentTime/ duration ) * 100;
+     const percent = (currentTime / duration ) * 100;
      $('#time-control .current-time').text( currentTime );
      $('#time-control input').val(percent);
       }, 1000);
